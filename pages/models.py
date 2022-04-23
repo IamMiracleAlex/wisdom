b0c8a97162771becec8c5d160c56d372acd5b051
Miracle <collinsalex50@gmail.com>
2022-04-23 14:12:20 +0100

    fix templates and admin

--- pages/models.py
+++ pages/models.py
@@ -3,7 +3,11 @@ from django.db import models
 
 class Quote(models.Model):
 	author = models.ForeignKey('users.User', on_delete=models.SET_NULL, null=True)
+	title = models.CharField(max_length=250, default='')
 	body = models.TextField()
 	make_public = models.BooleanField(default=False)
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self) -> str:
+		return self.title
--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.admin import UserAdmin
+from django.contrib import admin
+
+from users.models import User
 
 
+@admin.register(User)
 class CustomUserAdmin(UserAdmin):
 	pass
