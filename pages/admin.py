7bc13a3c6b11d48d69ea57154d9828c5ef1105fc
Miracle <collinsalex50@gmail.com>
2022-08-06 19:49:00 +0100

    added search and filter

--- pages/admin.py
+++ pages/admin.py
@@ -7,4 +7,7 @@ from pages.forms import QuoteAdminForm
 @admin.register(Quote)
 class QuoteAdmin(admin.ModelAdmin):
 	form = QuoteAdminForm
-	list_display = ['title', 'make_public', 'views', 'author', 'created_at'] 
+	list_display = ['title', 'make_public', 'views', 'author', 'created_at']
+	list_filter = ['make_public', 'author',]
+	search_fields = ['title', 'body']
+	date_hierarchy = ['created_at']
